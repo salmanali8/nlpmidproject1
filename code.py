ea75913ebb8928e442747d75dd59e376c11c138e
Salman Ali <92622212+salmanali8@users.noreply.github.com>
2023-11-22 21:29:37 +0500

    code.py

--- code.py
+++ code.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
 import streamlit as st
-
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -12,43 +8,25 @@ gauth = GoogleAuth()
 gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
 drive = GoogleDrive(gauth)
 
-# Function to get file from Google Drive
-def get_file_from_drive(file_id):
+# Function to download file from Google Drive using file ID
+def download_file(file_id):
     downloaded = drive.CreateFile({'id': file_id})
-    downloaded.GetContentFile('bbc-text.csv')  # Save file locally as 'bbc-text.csv'
-    return pd.read_csv('bbc-text.csv')
+    downloaded.GetContentFile('data.csv')  # Save file locally as 'data.csv'
+    return pd.read_csv('data.csv')
 
 # Streamlit app starts here
 st.title('Text Category Prediction')
 st.write('Google Drive Dataset Access')
 
-# File ID of your dataset in Google Drive
-file_id = 'YOUR_FILE_ID_HERE'  # Replace with the actual file ID from your Google Drive
+# File ID from the provided link
+file_id = 'https://drive.google.com/file/d/1cTBJJucUKfaCtr2Jo-oJK9nLILSRht_N/view?usp=drive_link'  # Replace with the file ID from your link
 
 try:
-    # Load data from Google Drive
-    df = get_file_from_drive(file_id)
+    # Load data from Google Drive using the file ID
+    df = download_file(file_id)
 
     # Assuming your CSV has two columns: 'text' and 'category'
     X = df['text']
     y = df['category']
 
-    # Create a text classification pipeline using TF-IDF vectorization and Linear SVM
-    model = make_pipeline(TfidfVectorizer(), SVC(kernel='linear'))
-
-    # Train the model
-    model.fit(X, y)
-
-    st.write('Data loaded from Google Drive and model trained!')
-
-    # Text input for prediction
-    st.write('Enter a sentence to predict its category:')
-    user_input = st.text_input('Enter a sentence:', '')
-
-    # Make predictions on the user input
-    if user_input:
-        prediction = model.predict([user_input])
-        st.write(f'Predicted Category: {prediction[0]}')
-except Exception as e:
-    st.write("Error:", e)
-
+    # Rest of your code for model training and prediction remains the same...
