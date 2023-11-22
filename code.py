c52abecf0341c88e8e81219c121ff0d0441fb7bb
Salman Ali <92622212+salmanali8@users.noreply.github.com>
2023-11-22 21:43:21 +0500

    code.py

--- code.py
+++ code.py
@@ -1,41 +1,29 @@
 import pandas as pd
 import streamlit as st
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-# Authenticate and create PyDrive client
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
-drive = GoogleDrive(gauth)
-
-# Function to download file from Google Drive using file ID
-def download_file(file_id):
-    downloaded = drive.CreateFile({'id': file_id})
-    downloaded.GetContentFile('data.csv')  # Save file locally as 'data.csv'
-    return pd.read_csv('data.csv')
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
 
 # Streamlit app starts here
 st.title('Text Category Prediction')
-st.write('Google Drive Dataset Access')
+st.write('GitHub Dataset Access')
 
-# File ID from the provided link
-file_id = 'https://drive.google.com/file/d/1cTBJJucUKfaCtr2Jo-oJK9nLILSRht_N/view?usp=drive_link'  # Replace with the file ID from your link
+# Raw URL of the CSV file in your GitHub repository
+file_url = 'https://raw.githubusercontent.com/salmanali8/nlpmidproject1/CODE-WITH-SA/bbc-text.csv'  # Replace with your raw CSV file URL
 
 try:
-    # Load data from Google Drive using the file ID
-    df = download_file(file_id)
+    # Load data from GitHub URL
+    df = pd.read_csv(file_url)
 
     # Assuming your CSV has two columns: 'text' and 'category'
     X = df['text']
     y = df['category']
 
     # Create a text classification pipeline using TF-IDF vectorization and Linear SVM
-    # For example:
-    # from sklearn.feature_extraction.text import TfidfVectorizer
-    # from sklearn.svm import SVC
-    # from sklearn.pipeline import make_pipeline
-    # model = make_pipeline(TfidfVectorizer(), SVC(kernel='linear'))
-    # model.fit(X, y)
+    model = make_pipeline(TfidfVectorizer(), SVC(kernel='linear'))
+
+    # Train the model
+    model.fit(X, y)
 
     # Text input for prediction
     st.write('Enter a sentence to predict its category:')
@@ -43,10 +31,8 @@ try:
 
     # Make predictions based on user input
     if user_input:
-        # Prediction logic with the trained model (adjust based on your model)
-        # prediction = model.predict([user_input])
-        # st.write(f'Predicted Category: {prediction[0]}')
-        st.write("Prediction functionality needs to be implemented.")
+        prediction = model.predict([user_input])
+        st.write(f'Predicted Category: {prediction[0]}')
 
 except Exception as e:
     st.write("Error:", e)
