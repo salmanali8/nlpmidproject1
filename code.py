4618e5ab5dd09c78792e2427352c578af87f5906
Salman Ali <92622212+salmanali8@users.noreply.github.com>
2023-11-22 21:21:38 +0500

    code.py

--- code.py
+++ code.py
@@ -2,31 +2,52 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
-
 import streamlit as st
 
-# Load the CSV file
-file_path = '/content/drive/My Drive/dataset2/bbc-text.csv'
-df = pd.read_csv(file_path)
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
-# Assuming your CSV has two columns: 'text' and 'category'
-X = df['text']
-y = df['category']
+# Authenticate and create PyDrive client
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
+drive = GoogleDrive(gauth)
 
-# Create a text classification pipeline using TF-IDF vectorization and Linear SVM
-model = make_pipeline(TfidfVectorizer(), SVC(kernel='linear'))
-
-# Train the model
-model.fit(X, y)
+# Function to get file from Google Drive
+def get_file_from_drive(file_id):
+    downloaded = drive.CreateFile({'id': file_id})
+    downloaded.GetContentFile('bbc-text.csv')  # Save file locally as 'bbc-text.csv'
+    return pd.read_csv('bbc-text.csv')
 
 # Streamlit app starts here
 st.title('Text Category Prediction')
-st.write('Enter a sentence to predict its category:')
+st.write('Google Drive Dataset Access')
 
-# Text input for prediction
-user_input = st.text_input('Enter a sentence:', '')
+# File ID of your dataset in Google Drive
+file_id = 'YOUR_FILE_ID_HERE'  # Replace with the actual file ID from your Google Drive
 
-# Make predictions on the user input
-if user_input:
-    prediction = model.predict([user_input])
-    st.write(f'Predicted Category: {prediction[0]}')
+try:
+    # Load data from Google Drive
+    df = get_file_from_drive(file_id)
+
+    # Assuming your CSV has two columns: 'text' and 'category'
+    X = df['text']
+    y = df['category']
+
+    # Create a text classification pipeline using TF-IDF vectorization and Linear SVM
+    model = make_pipeline(TfidfVectorizer(), SVC(kernel='linear'))
+
+    # Train the model
+    model.fit(X, y)
+
+    st.write('Data loaded from Google Drive and model trained!')
+
+    # Text input for prediction
+    st.write('Enter a sentence to predict its category:')
+    user_input = st.text_input('Enter a sentence:', '')
+
+    # Make predictions on the user input
+    if user_input:
+        prediction = model.predict([user_input])
+        st.write(f'Predicted Category: {prediction[0]}')
+except Exception as e:
+    st.write("Error:", e)
